eff31a347881534bcea9f1d001161d85bfb38a81
Stealth <mr.duedull@gmail.com>
2025-07-23 20:04:36 +0530

    learnt datatypes

---
+++ ts/datatypes.ts
@@ -0,0 +1,17 @@
+const add = (a:number, b:number):number => {
+    return a+b;
+}
+
+let run:boolean = true;
+
+while(run){
+    console.log(`welcome to the calculator\nchoose action you want to perform`);
+    console.log(`1. addition
+                 2.Substraction
+                 3. Multiplication
+                 4. division`);
+
+                 
+}    
+
+            
--- ts/index.ts
+++ ts/index.ts
@@ -7,21 +7,21 @@
 
 // newOne1(a1,a2);
 
+//data types in js, Implementing in ts
+
+//PRIMITIVE
+// 1. number
 let a:number = 3_4_3;
 console.log(a);
 
+//number - bigint
 let b:bigint = 334543534_44554645_34353534n;
 console.log(b);
 
+//3. string
 let sent:string = "this is a string";
 console.log(sent);
 
-let arr:number[] = [2,4,5,9];
-console.log(arr);
-
-
-let char_arr:string[] = ['this','is','a','string array'];
-console.log(char_arr);
 
 //null and undefined types
 let first:undefined = undefined;
@@ -29,6 +29,23 @@ console.log(first);
 
 let second:null = null;
 console.log(second);
+
+//4. boolean
+let booldata: boolean = false;
+
+//5. symbol
+const symbolVar: symbol = Symbol("unique");
+const symbolVar2: symbol = Symbol("unique");
+console.log(symbolVar);
+console.log(symbolVar==symbolVar2);
+
+// NON PRIMITIVE
+//1. arrays
+let arr:number[] = [2,4,5,9];
+console.log(arr);
+
+let char_arr:string[] = ['this','is','a','string array'];
+console.log(char_arr);
 
 //objects
 let firstObj:object = {
@@ -40,6 +57,7 @@ console.log(firstObj);
 let secondObj:object = {...firstObj, gender:'male'};
 console.log(secondObj);
 
+//creating a blueprint interface for creating objects
 interface User {
     name: string,
     age: number,
@@ -52,5 +70,41 @@ let userObj: User = {
     gender: 'female'
 }
 console.log(userObj);
+
+let obj3: {name: string, school_id: string, phone: number} = {
+    name: "ssps",
+    school_id: '454df',
+    phone: 3457564
+}
+console.log(obj3);
+
+//ts datatypes 
+// 1. any - Opts out of type checking for a variable, allowing it to hold any type of value. Use with caution.
+// (by default when we do not specify any type it becomes any type)
+let tempvar:any = 343;
+console.log(tempvar);
+tempvar = 'aj';
+console.log(tempvar);
+
+//2. tuple - Tuple types allow you to express an array with a fixed number of elements whose types are known, but need not be the same.
+let tupleVar: [string, number,boolean] = [ `string here`, 44, true];
+console.log(tupleVar);
+
+// 3. ENUMS
+enum colorEnum {
+    RED = 3,
+    BLUE,
+    GREEN,
+    PEACH,
+    ORANGE
+} 
+
+let c: colorEnum = colorEnum.GREEN;
+let d: string = colorEnum[3];
+console.log(c);
+console.log(d);
+
+
+
 
 
